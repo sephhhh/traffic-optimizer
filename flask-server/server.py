80e892f607755b3dad4a424d8da8a7e2a7ae5b28
sephhhh <stephenwu07curry@gmail.com>
2025-03-01 14:41:11 -0500

    1

--- flask-server/server.py
+++ flask-server/server.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/members")
+@app.route("/")
 def members():
-  return {"members": ["hi", "h2", "h3"]}
+  return "3212"
 
 if __name__ == "__main__":
   app.run(debug=True)
