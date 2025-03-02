f8a92c59488a20bc2dbe9f9a4365fbd40064be86
sephhhh <stephenwu07curry@gmail.com>
2025-03-01 19:59:48 -0500

    finished styling the custom file input container

--- client/src/components/dropzone.js
+++ client/src/components/dropzone.js
@@ -35,42 +35,44 @@ const Dropzone = ({className}) => {
 
   return (
     <div className='custom-file-box'>
-    <form className='custom-file-label'>
-      <div className='box' {...getRootProps({
-        className: className
-      })}>
-        <input {...getInputProps()} />
-        {isDragActive ? (
-          <p>Drop the files here ...</p>
-        ) : (
-          <div class='custom-file-input-label'>
-            <img src={DocumentImg} alt='document' id='documentSvg'/>
-            <p>Drag and drop some files here or</p>
-            <button type='button' id='uploadBtn'>
-              Upload
-              <img src={DownArrow} id='downArrow'/>
-              </button>
-          </div>
-        )}
+      <form className='custom-file-label'>
+        <div className='box' {...getRootProps({
+          className: className
+        })}>
+          <input {...getInputProps()} />
+          {isDragActive ? (
+            <p>Drop the files here ...</p>
+          ) : (
+            <div class='custom-file-input-label'>
+              <img src={DocumentImg} alt='document' id='documentSvg'/>
+              <p className='custom-file-input-text'>Drop your files here or</p>
+              <button type='button' id='uploadBtn'>
+                Upload
+                <img src={DownArrow} id='downArrow'/>
+                </button>
+            </div>
+          )}
+        </div>
+      </form>
+      <div style={{width: "93%"}}>
+        <ul className='acceptedFiles'>
+        {files.map(file => {
+          return (
+          <li key={file.name}>
+            {file.name}
+            <button type='button' onClick={() => removeFile(file.name)}>X</button>
+          </li>
+          )
+        })}
+      </ul>
       </div>
-    </form>
-          <ul>
-          {files.map(file => {
-            return (
-            <li key={file.name}>
-              {file.name}
-              <button type='button' onClick={() => removeFile(file.name)}>X</button>
-            </li>
-            )
-          })}
-        </ul>
-          <ul className='mt-6 flex flex-col'>
-          {rejected.length > 0 && (
-    <div>
-      <p>Last rejected file: {rejected[rejected.length - 1].file.name}</p>
-    </div>
-  )}
-          </ul>
+    <ul className='mt-6 flex flex-col'>
+            {rejected.length > 0 && (
+      <div>
+        <p>Last rejected file: {rejected[rejected.length - 1].file.name}</p>
+      </div>
+    )}
+    </ul>
   </div>
   )
 }
